a93d6d6d3b81d6af74ef1f08b73bad51046ac85d
TaleRG <talesrodriguesgoncalves98@gmail.com>
2024-08-07 06:49:06 -0300

    feat(config): adiciona logs

--- echoeasy/src/service/usuario.service.ts
+++ echoeasy/src/service/usuario.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Usuario } from '../schema/Usuario';
@@ -6,12 +6,15 @@ import { UsuarioDto } from '../dto/UsuarioDto';
 
 @Injectable()
 export class UsuarioService {
+  private readonly logger = new Logger(UsuarioService.name);
   constructor(
     @InjectModel(Usuario.name) private usuarioModel: Model<Usuario>,
   ) {}
 
   async create(usuarioData: UsuarioDto): Promise<Usuario> {
+    this.logger.log('Inicializando criação de usuário...');
     const usuario = new this.usuarioModel(usuarioData);
+    this.logger.log('Finalizando criação de usuário...');
     return usuario.save();
   }
 
