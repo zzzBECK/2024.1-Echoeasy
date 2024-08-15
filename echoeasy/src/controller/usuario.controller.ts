f56ae8b92bb24bbeef4d284f2502a4536a49c000
Alexandre Beck <alexandrebeck2003@gmail.com>
2024-08-15 17:31:33 -0300

    feat: Add authentication guard to UsuarioController

--- echoeasy/src/controller/usuario.controller.ts
+++ echoeasy/src/controller/usuario.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
+import { AuthGuard } from 'src/guards/auth.guard';
 import { Usuario } from '../schema/Usuario';
 import { UsuarioService } from '../service/usuario.service';
 
@@ -7,11 +8,13 @@ export class UsuarioController {
   constructor(private usuarioService: UsuarioService) {}
 
   @Get()
+  @UseGuards(AuthGuard)
   async getUsuarios(): Promise<Usuario[]> {
     return this.usuarioService.findAll();
   }
 
   @Get('search')
+  @UseGuards(AuthGuard)
   async findUsuarioByEmail(
     @Query('email') email: string,
   ): Promise<Usuario | null> {
