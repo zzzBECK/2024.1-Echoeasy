f11b1166c9bcc7977b80cc58ba48395b841e862c
Lucas Gabriel da Silva Antunes <190091681@aluno.unb.br>
2024-08-16 11:09:26 -0300

    chore: Add authentication to Documento and Assunto (#36)

--- echoeasy/src/controller/assunto.controller.ts
+++ echoeasy/src/controller/assunto.controller.ts
@@ -6,26 +6,31 @@ import {
   Query,
   Delete,
   Put,
+  UseGuards,
 } from '@nestjs/common';
 import { Assunto } from '../schema/Assunto';
 import { AssuntoService } from '../service/assunto.service';
 import { AssuntoDto } from '../dto/AssuntoDto';
+import { AuthGuard } from 'src/guards/auth.guard';
 
 @Controller('assuntos')
 export class AssuntoController {
   constructor(private assuntoService: AssuntoService) {}
 
   @Post()
+  @UseGuards(AuthGuard)
   async criarAssunto(@Body() assuntoData: AssuntoDto): Promise<Assunto> {
     return this.assuntoService.create(assuntoData);
   }
 
   @Get('all')
+  @UseGuards(AuthGuard)
   async getAssuntos(): Promise<Assunto[]> {
     return this.assuntoService.findAll();
   }
 
   @Get('search')
+  @UseGuards(AuthGuard)
   async findassuntoByTitle(
     @Query('title') title: string,
   ): Promise<Assunto | null> {
@@ -33,6 +38,7 @@ export class AssuntoController {
   }
 
   @Put('update')
+  @UseGuards(AuthGuard)
   async updateAssunto(
     @Query('title') title: string,
     @Body() assuntoData: AssuntoDto,
@@ -41,6 +47,7 @@ export class AssuntoController {
   }
 
   @Delete('delete')
+  @UseGuards(AuthGuard)
   async deleteAssuntoByTitle(
     @Query('title') title: string,
   ): Promise<Assunto | null> {
--- echoeasy/src/controller/documento.controller.ts
+++ echoeasy/src/controller/documento.controller.ts
@@ -6,16 +6,19 @@ import {
   Query,
   Delete,
   Put,
+  UseGuards,
 } from '@nestjs/common';
 import { Documento } from '../schema/Documento';
 import { DocumentoService } from '../service/documento.service';
 import { DocumentoDto } from '../dto/DocumentoDto';
+import { AuthGuard } from 'src/guards/auth.guard';
 
 @Controller('documentos')
 export class DocumentoController {
   constructor(private documentoService: DocumentoService) {}
 
   @Post()
+  @UseGuards(AuthGuard)
   async criarDocumento(
     @Body() documentoData: DocumentoDto,
   ): Promise<Documento> {
@@ -23,11 +26,13 @@ export class DocumentoController {
   }
 
   @Get('all')
+  @UseGuards(AuthGuard)
   async getDocumentos(): Promise<Documento[]> {
     return this.documentoService.findAll();
   }
 
   @Get('search')
+  @UseGuards(AuthGuard)
   async findDocumentoByTitle(
     @Query('title') title: string,
   ): Promise<Documento | null> {
@@ -35,6 +40,7 @@ export class DocumentoController {
   }
 
   @Put('update')
+  @UseGuards(AuthGuard)
   async updateDocumento(
     @Query('title') title: string,
     @Body() documentoData: DocumentoDto,
@@ -43,6 +49,7 @@ export class DocumentoController {
   }
 
   @Delete('delete')
+  @UseGuards(AuthGuard)
   async deleteDocumentoByTitle(
     @Query('title') title: string,
   ): Promise<Documento | null> {
