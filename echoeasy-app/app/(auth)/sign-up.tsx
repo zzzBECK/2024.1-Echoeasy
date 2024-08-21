d43ab1e877d88c0acee5016f810bcfcd2f52284b
Leandro Almeida <leo.leandro.ars@gmail.com>
2024-08-21 12:09:44 -0300

    feat: adds validation to signup page

--- echoeasy-app/app/(auth)/sign-up.tsx
+++ echoeasy-app/app/(auth)/sign-up.tsx
@@ -1,24 +1,30 @@
 import { Link, router } from "expo-router";
 import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { ScrollView, KeyboardAvoidingView, Platform, Text, View } from "react-native";
+import { ScrollView, Text, View } from "react-native";
 import { UsuarioService } from "../service/UsuarioService";
 import { UserPayload } from "../types/User";
 import FormField from "../../components/FormField";
 import CustomButton from '../../components/CustomButton';
 import { Formik } from 'formik';
+import * as yup from 'yup';
 
 const SignUp: React.FC = () => {
   const [error, setError] = useState("");
   const [message, setMessage] = useState("");
   const usuarioService = new UsuarioService();
 
-  const handleSignUp = async (values: UserPayload) => {
-    if (values.name === "" || values.email === "" || values.phoneNumber === "" || values.password === "") {
-      setError("Todos os campos são obrigatórios");
-      return;
-    }
+  const signUpSchema = yup.object().shape({
+    name: yup.string().required('Nome é obrigatório'),
+    email: yup.string().email('E-mail inválido').required('E-mail é obrigatório'),
+    phoneNumber: yup.string().required('Número de telefone é obrigatório'),
+    password: yup.string().min(8, 'A senha deve ter no mínimo 8 caracteres').required('Senha é obrigatória'),
+    confirmPassword: yup.string()
+      .oneOf([yup.ref('password'), ""], 'As senhas devem coincidir')
+      .required('Confirmação de senha é obrigatória'),
+  });
 
+  const handleSignUp = async (values: UserPayload) => {
     try {
       const usuario = await usuarioService.create(values);
       console.log(usuario);
@@ -32,90 +38,96 @@ const SignUp: React.FC = () => {
 
   return (
     <SafeAreaView className="bg-[#F6F6F6] h-full">
-     
-        <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
-          <View className="w-full h-full flex justify-center items-center p-4">
-            <Formik
-              initialValues={{ name: '', email: '', phoneNumber: '', password: '', confirmPassword: '' }}
-              onSubmit={(values) => {
-                handleSignUp(values);
-              }}
-            >
-              {({ handleChange, handleBlur, handleSubmit, values }) => (
-                <>
-                  <Text className="text-2xl font-interMedium text-center">Cadastre-se</Text>
 
-                  <View className="my-5">
-                    <FormField
-                      label="Nome"
-                      icon="person-outline"
-                      placeholder="Digite seu nome e sobrenome"
-                      value={values.name}
-                      onChangeText={handleChange('name')}
-                      onBlur={handleBlur('name')}
-                    />
-                    <FormField
-                      label="E-mail"
-                      icon="mail-outline"
-                      placeholder="Digite seu e-mail"
-                      value={values.email}
-                      onChangeText={handleChange('email')}
-                      onBlur={handleBlur('email')}
-                    />
-                    <FormField
-                      label="Telefone"
-                      icon="call-outline"
-                      placeholder="Digite seu número de telefone"
-                      value={values.phoneNumber}
-                      onChangeText={handleChange('phoneNumber')}
-                      onBlur={handleBlur('phoneNumber')}
-                    />
-                    <FormField
-                      label="Senha"
-                      icon="lock-closed-outline"
-                      placeholder="Crie uma senha segura"
-                      secureTextEntry
-                      value={values.password}
-                      onChangeText={handleChange('password')}
-                      onBlur={handleBlur('password')}
-                    />
-                    <FormField
-                      label="Confirmação de Senha"
-                      icon="checkmark-outline"
-                      placeholder="Confirme sua senha"
-                      secureTextEntry
-                      value={values.confirmPassword}
-                      onChangeText={handleChange('confirmPassword')}
-                      onBlur={handleBlur('confirmPassword')}
-                    />
-                  </View>
+      <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+        <View className="w-full h-full flex justify-center items-center p-4">
+          <Formik
+            initialValues={{ name: '', email: '', phoneNumber: '', password: '', confirmPassword: '' }}
+            validationSchema={signUpSchema}
+            onSubmit={(values) => {
+              handleSignUp(values);
+            }}
+          >
+            {({ handleChange, handleBlur, handleSubmit, values, errors, touched }) => (
+              <>
+                <Text className="text-2xl font-interMedium text-center">Cadastre-se</Text>
 
-                  <Text className="text-center font-interRegular mb-4 mx-4">
-                    <Text>Ao se cadastrar, você concorda com os nossos </Text>
-                    <Link href="/#" className="text-[#209B85]">Termos</Link>
-                    <Text> e </Text>
-                    <Link href="/#" className="text-[#209B85]">Política de Privacidade</Link>
-                    <Text>.</Text>
-                  </Text>
+                <View className="my-5">
+                  <FormField
+                    label="Nome"
+                    icon="person-outline"
+                    placeholder="Digite seu nome e sobrenome"
+                    value={values.name}
+                    onChangeText={handleChange('name')}
+                    onBlur={handleBlur('name')}
+                    error={touched.name && errors.name ? errors.name : ""}
+                  />
+                  <FormField
+                    label="E-mail"
+                    icon="mail-outline"
+                    placeholder="Digite seu e-mail"
+                    value={values.email}
+                    onChangeText={handleChange('email')}
+                    onBlur={handleBlur('email')}
+                    error={touched.email && errors.email ? errors.email : ""}
+                  />
+                  <FormField
+                    label="Telefone"
+                    icon="call-outline"
+                    placeholder="Digite seu número de telefone"
+                    value={values.phoneNumber}
+                    onChangeText={handleChange('phoneNumber')}
+                    onBlur={handleBlur('phoneNumber')}
+                    error={touched.phoneNumber && errors.phoneNumber ? errors.phoneNumber : ""}
+                  />
+                  <FormField
+                    label="Senha"
+                    icon="lock-closed-outline"
+                    placeholder="Crie uma senha segura"
+                    secureTextEntry
+                    value={values.password}
+                    onChangeText={handleChange('password')}
+                    onBlur={handleBlur('password')}
+                    error={touched.password && errors.password ? errors.password : ""}
+                  />
+                  <FormField
+                    label="Confirmação de Senha"
+                    icon="checkmark-outline"
+                    placeholder="Confirme sua senha"
+                    secureTextEntry
+                    value={values.confirmPassword}
+                    onChangeText={handleChange('confirmPassword')}
+                    onBlur={handleBlur('confirmPassword')}
+                    error={touched.confirmPassword && errors.confirmPassword ? errors.confirmPassword : ""}
+                  />
+                </View>
 
-                  <CustomButton title="Confirmar" active={true} isLoading={false} onPressProps={handleSubmit} />
+                <Text className="text-center font-interRegular mb-4 mx-4">
+                  <Text>Ao se cadastrar, você concorda com os nossos </Text>
+                  <Link href="/#" className="text-[#209B85]">Termos</Link>
+                  <Text> e </Text>
+                  <Link href="/#" className="text-[#209B85]">Política de Privacidade</Link>
+                  <Text>.</Text>
+                </Text>
 
-                  {error ? (
-                    <Text className="text-red-500 text-center mt-4">{error}</Text>
-                  ) : null}
+                <CustomButton title="Confirmar" active={true} isLoading={false} onPressProps={handleSubmit} />
 
-                  <View className="flex-row justify-center mt-4">
-                    <Text className='font-interRegular text-base'>Já tem uma conta? </Text>
-                    <Link href="/sign-in" className="font-interBold text-base text-[#209B85]">
-                      Conecte-se
-                    </Link>
-                  </View>
-                </>
-              )}
-            </Formik>
-          </View>
-        </ScrollView>
-     
+                {error ? (
+                  <Text className="text-red-500 text-center mt-4">{error}</Text>
+                ) : null}
+
+                <View className="flex-row justify-center mt-4">
+                  <Text className='font-interRegular text-base'>Já tem uma conta? </Text>
+                  <Link href="/sign-in" className="font-interBold text-base text-[#209B85]">
+                    Conecte-se
+                  </Link>
+                </View>
+              </>
+            )}
+          </Formik>
+        </View>
+      </ScrollView>
+
     </SafeAreaView>
   );
 };
--- echoeasy-app/components/FormField.tsx
+++ echoeasy-app/components/FormField.tsx
@@ -1,24 +1,31 @@
 import React from 'react';
 import { View, TextInput, Text, TextInputProps } from 'react-native';
 import Ionicons from '@expo/vector-icons/Ionicons';
+import { NativeSyntheticEvent, TextInputFocusEventData } from 'react-native';
 
 interface FormFieldProps extends TextInputProps {
   label?: string;
   placeholder?: string;
   icon?: React.ComponentProps<typeof Ionicons>['name'];
+  value: string,
   error?: string;
+  onChangeText: (text: string) => void;
+  onBlur: (e: NativeSyntheticEvent<TextInputFocusEventData>) => void;
 }
 
-const FormField: React.FC<FormFieldProps> = ({ label, placeholder, icon, error }) => {
+const FormField: React.FC<FormFieldProps> = ({ label, placeholder, icon, value, error, onChangeText, onBlur, ...rest }) => {
   return (
     <View className='space-y-1 my-1'>
       <Text className="text-base font-interRegular">{label}</Text>
-      <View className="w-80 h-14 px-3 bg-[#FFFFFF] rounded-2xl border border-[#E3E3E3] flex flex-row items-center focus:border-[#333333] ">
+      <View className={`w-80 h-14 px-3 bg-[#FFFFFF] rounded-2xl border border-[#E3E3E3] flex flex-row items-center focus:border-[#333333] ${error ? 'border-red-500' : 'border-[#E3E3E3]'}`}>
       {icon && <Ionicons name={icon} size={25} color="#C4C4C4"/>} 
         <TextInput
           className="flex-1 ml-2 text-[#A4A4A4] font-interRegular text-base"
           placeholder={placeholder}
-          
+          value={value}
+          onChangeText={onChangeText}
+          onBlur={onBlur}
+          {...rest}
         />
       </View>
       {error ? <Text className="text-red-500 mt-1">{error}</Text> : null}
